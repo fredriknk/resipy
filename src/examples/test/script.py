0a87279ed41a59fcdd26aef2eea914e0eb1576fb
nmbu_linux <fredrik.kleven@outlook.com>
2022-04-01 14:11:21 +0200

    hannahs script

--- src/examples/test/script.py
+++ src/examples/test/script.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     # k.showPseudo3DMesh(cropMesh=True)
     k.param['b_wgt'] = 0.05
     k.invertPseudo3D(runParallel=True)
-    k.showResults(index=-12, cropMesh=False, color_map='jet', vmin=0.8, vmax=4,cropMaxDepth=False,clipContour=False)
+    k.showResults(index=-1, cropMesh=False, color_map='jet', vmin=0.8, vmax=4,cropMaxDepth=False,clipContour=False,contour=True,background_color=(1.,1.,1.))
     timings['Pseudo 3D'] = time.time() - t0
     # k.saveProject(r'/home/fredrikfnk/PycharmProjects/resipy/src/examples/test/JN_Pseudo3D_Willington.resipy')
     #
