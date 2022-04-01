3dd2bd3cf83bce18a0da7eb568479912b09ace29
nmbu_linux <fredrik.kleven@outlook.com>
2022-04-01 09:50:44 +0200

    hannahs script

---
+++ src/examples/test/script.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.append("/home/fredrikfnk/PycharmProjects/resipy/src")
+from resipy import Project
+import pyvista as pv
+import time
+
+
+if __name__ == '__main__':
+
+    timings = {}
+    t0 = time.time()
+    k = Project(typ='R2')
+    k.createPseudo3DSurvey(r'/home/fredrikfnk/PycharmProjects/resipy/src/examples/test/data',
+                           lineSpacing=20, ftype="ResInv")
+    k.importPseudo3DElec(r'/home/fredrikfnk/PycharmProjects/Resipy_interpolate/topofiles/electrodes3d.csv')
+    k.createMultiMesh(typ='trian', show_output=False, dump=None, runParallel=True)
+    # k.showPseudo3DMesh(cropMesh=True)
+    k.param['b_wgt'] = 0.05
+    k.invertPseudo3D(runParallel=True)
+    k.showResults(index=-12, cropMesh=False, color_map='jet', vmin=0.8, vmax=4,cropMaxDepth=False,clipContour=False)
+    timings['Pseudo 3D'] = time.time() - t0
+    # k.saveProject(r'/home/fredrikfnk/PycharmProjects/resipy/src/examples/test/JN_Pseudo3D_Willington.resipy')
+    #
+    # t1 = time.time()
+    # m = Project(typ='R3t')
+    # m.create3DSurvey(r'/home/fredrikfnk/PycharmProjects/resipy/src/examples/test/data',
+    #                        lineSpacing=20, ftype="ResInv")
+    #
+    # m.importElec(r'/home/fredrikfnk/PycharmProjects/Resipy_interpolate/topofiles/electrodes3d.csv')
+    #
+    # m.createMesh(typ='tetra', fmd=25, cl_factor=4, cl=1.5)
+    # m.showMesh()
+    #
+    # m.param['b_wgt'] = 0.05
+    #
+    #
+    # def dump(x):
+    #     pass
+    #
+    #
+    # m.invert(dump=dump)
+    # m.saveProject(r'/home/fredrikfnk/PycharmProjects/resipy/src/examples/test/JN_Real3D_Willington.resipy')
+    # m.showResults(color_map='jet', vmin=0.8, vmax=4)
+
